23aea746f1d9dfc035ef618d4c573590556a2323
Rani Shah <rani76@gmail.com>
2018-03-18 22:10:56 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,18 @@
 class School
 
-  attr_reader :name, :roster, :student
+  attr_reader :name, :roster, :grade
 
   def initialize(name, roster = {})
     @name = name
     @roster = roster
   end
 
+  def add_student(student_name, grade)
+    if roster[grade]
+      roster[grade] << student_name
+    else
+      roster[grade] = []
+      roster[grade] << student_name
+  end
 
 end
