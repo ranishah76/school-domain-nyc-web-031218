ed93b7e1d12abf85b8ec9691ea17593488571b6f
Rani Shah <rani76@gmail.com>
2018-03-18 22:06:05 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,28 +1,3 @@
-# code here!
 class School
 
-  attr_reader :name, :roster
-
- def initialize(name, roster = {})
-   @name = name
-   @roster = roster
- end
-
-def add_student(student_name, grade)
-  if roster[grade]
-    roster[grade] << student_name
-  else
-  roster[grade] = []
-  roster[grade] << student_name
-end
-end
-
-def grade(grade)
-  roster[grade]
-end
-
-def sort(roster)
-  roster.sort_by { |grade, student_name| student_name}
-end
-
-end
+end 
